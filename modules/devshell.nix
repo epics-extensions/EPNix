71fc89cf56dbb2da61cf104a314e77ed6eb14ba7
Minijackson <minijackson@riseup.net>
2021-09-23 08:51:22 +0200

    devshell: add shell commands lib

--- modules/devshell.nix
+++ modules/devshell.nix
@@ -17,7 +17,52 @@ with lib;
   };
 
   config.epnix.outputs.devShell = config.devShell.devshell.shell;
-  config.nixpkgs.overlays = [ devshell.overlay ];
+  config.nixpkgs.overlays = [
+    devshell.overlay
+    (self: super: {
+      epnix-commands-lib = super.writeTextDir "/libexec/epnix/commands-lib.sh" ''
+        set -euo pipefail
+        IFS=$'\n\t'
+
+        tput() {
+          # If tput fails, it just means less colors
+          ${pkgs.ncurses}/bin/tput "$@" 2> /dev/null || true
+        }
+
+        NORMAL="$(tput sgr0)"
+        BOLD="$(tput bold)"
+
+        RED="$(tput setaf 1)"
+        GREEN="$(tput setaf 2)"
+        YELLOW="$(tput setaf 3)"
+        BLUE="$(tput setaf 4)"
+        PURPLE="$(tput setaf 5)"
+        CYAN="$(tput setaf 6)"
+        WHITE="$(tput setaf 7)"
+
+        echoe() {
+          echo "$@" >&2
+        }
+
+        info() {
+          echoe "$BOLD''${CYAN}Info: $WHITE$@$NORMAL"
+        }
+
+        warn() {
+          echoe "$BOLD''${YELLOW}Warning: $WHITE$@$NORMAL"
+        }
+
+        error() {
+          echoe "$BOLD''${RED}Error: $WHITE$@$NORMAL"
+        }
+
+        fatal() {
+          error "$@"
+          exit 1
+        }
+      '';
+    })
+  ];
 
   config.devShell.devshell.motd = ''
     [38;5;202m🔨 Welcome to EPNix-${config.epnix.buildConfig.flavor}'s devShell[0m
