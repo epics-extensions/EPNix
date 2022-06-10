aa4b5172ce16d824e4717adb503d5de4b66d5fc0
Minijackson <minijackson@riseup.net>
2022-06-10 11:12:27 +0200

    mkEpicsPackage: don't remove things from the build arch when cross
    This is beceause packages that depend on epics-base, like asyn, modbus,
    etc. will also try to compile build -> host *and* build -> build, the
    latter needing libs and stuff from the build architecture...

--- pkgs/build-support/mk-epics-package.nix
+++ pkgs/build-support/mk-epics-package.nix
@@ -130,16 +130,6 @@ in
             done
           fi
         ''
-        # When cross-compiling, EPICS actually compiles everything for the build
-        # platform *and* for the host platform. We don't need products for the
-        # build platform, so we remove them.
-        #
-        # TODO: find a solution upstream so that we don't waste time compiling for
-        # the build platform.
-        + (optionalString (stdenv.buildPlatform != stdenv.hostPlatform)
-          ''
-            rm -rf $out/{bin,lib}/${build_arch}
-          '')
         + postInstall;
 
       doCheck = attrs.doCheck or true;
