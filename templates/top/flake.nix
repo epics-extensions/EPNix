6ea655ba46987219189425f6fe5f726a6f4660cc
Minijackson <minijackson@riseup.net>
2022-09-13 11:39:52 +0200

    templates/top: add commented example of cross-compilation
    Fixes #21

--- templates/top/flake.nix
+++ templates/top/flake.nix
@@ -60,8 +60,17 @@
     # environment can be built on your machine.
     flake-utils.lib.eachSystem ["x86_64-linux"] (system:
       with epnix.lib; let
+        inherit (epnix.inputs) nixpkgs;
+
         result = evalEpnixModules {
-          nixpkgsConfig.system = system;
+          nixpkgsConfig = {
+            # This specifies the build architecture
+            inherit system;
+
+            # This specifies the host architecture, uncomment for cross-compiling
+            # ---
+            #crossSystem = nixpkgs.lib.systems.examples.armv7l-hf-multiplatform;
+          };
           epnixConfig = myEpnixDistribution;
         };
       in {
