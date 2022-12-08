29ea863e6c549d24ecf6889622d653e789dd3e33
Minijackson <minijackson@riseup.net>
2022-12-08 09:21:29 +0100

    checks: remove unneeded apps and iocboots

--- checks/support/seq/simple/top/epnix.nix
+++ checks/support/seq/simple/top/epnix.nix
@@ -3,8 +3,6 @@
     meta.name = "checks-support-seq-simple";
     buildConfig.src = ./.;
 
-    applications.apps = [./simpleApp];
-    boot.iocBoots = [./iocBoot/iocsimple];
     support.modules = with pkgs.epnix.support; [seq];
 
     nixos.service.app = "simple";
