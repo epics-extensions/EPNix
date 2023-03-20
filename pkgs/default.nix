c446fb14d19f3cd7ce2183d3a2e73f8fc902cd67
Minijackson <minijackson@riseup.net>
2023-03-20 14:54:26 +0100

    phoebus-olog: init at 4.7.3

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,9 @@ with prev;
     mkEpicsPackage = callPackage ./build-support/mk-epics-package.nix {};
 
     epnix = recurseIntoAttrs rec {
+
+      # EPICS base
+
       epics-base7 = callPackage ./epnix/epics-base {
         version = "7.0.7";
         hash = "sha256-VMiuwTuPykoMLcIphUAsjtLQZ8HLKr3LvGpje3lsIXc=";
@@ -15,6 +18,9 @@ with prev;
         hash = "sha256-QWScmCEaG0F6OW6LPCaFur4W57oRl822p7wpzbYhOuA=";
       };
       epics-base = epics-base7;
+
+      # EPICS support modules
+
       support = recurseIntoAttrs {
         asyn = callPackage ./epnix/support/asyn {};
         calc = callPackage ./epnix/support/calc {};
@@ -27,6 +33,9 @@ with prev;
         StreamDevice = callPackage ./epnix/support/StreamDevice {};
       };
 
+      # EPICS related tools and extensions
+
+      phoebus-olog = callPackage ./epnix/tools/phoebus/olog {};
       procServ = callPackage ./epnix/tools/procServ {};
     };
   }
---
+++ pkgs/epnix/tools/phoebus/olog/default.nix
@@ -0,0 +1,103 @@
+{
+  lib,
+  epnixLib,
+  stdenv,
+  fetchFromGitHub,
+  maven,
+  libfaketime,
+  canonicalize-jars-hook,
+  jdk,
+  git,
+  makeWrapper,
+}: let
+  # TODO: upstream outputTimestamp
+  buildDate = "2022-02-24T07:56:00Z";
+  mvnOptions = "-Dmaven.javadoc.skip=true -Dmaven.source.skip=true -Pdeployable-jar -Dproject.build.outputTimestamp=${buildDate}";
+in
+  stdenv.mkDerivation (final: {
+    pname = "phoebus-olog";
+    version = "4.7.3";
+
+    src = fetchFromGitHub {
+      owner = "Olog";
+      repo = "phoebus-olog";
+      rev = "v${final.version}";
+      hash = "sha256-WwRB4QtZBeH6GptTZJ02CBpP7BGzjZbwMYQrOmGevFo=";
+    };
+
+    deps = stdenv.mkDerivation {
+      name = with final; "${pname}-${version}-deps";
+      inherit (final) src;
+
+      nativeBuildInputs = [jdk maven git];
+
+      buildPhase = ''
+        runHook preBuild
+
+        # Don't use the launch script, we use the jar file as a jar
+        mvn package ${mvnOptions} -Dmaven.repo.local=$out
+
+        runHook postBuild
+      '';
+
+      # keep only *.{pom,jar,sha1,nbm} and delete all ephemeral files with
+      # lastModified timestamps inside
+      installPhase = ''
+        runHook preInstall
+
+        find $out -type f \
+          -name \*.lastUpdated -or \
+          -name resolver-status.properties -or \
+          -name _remote.repositories \
+          -delete
+
+        runHook postInstall
+      '';
+
+      outputHashAlgo = "sha256";
+      outputHashMode = "recursive";
+      outputHash = "sha256-feT4qSN7B+KSUIoCmBkjDjsib39QfUW/4PxsU3Jx2Gs=";
+
+      doCheck = false;
+    };
+
+    nativeBuildInputs = [maven makeWrapper libfaketime];
+
+    buildPhase = ''
+      runHook preBuild
+
+      # Use faketime because files inside the jar have an mtime
+      faketime -f "1970-01-01 00:00:01" mvn package --offline ${mvnOptions} -Dmaven.repo.local="$deps"
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/bin
+      mkdir -p $out/share/java
+
+      jarName="service-olog-${final.version}.jar"
+
+      classpath=$(find $deps -name "*.jar" -printf ':%h/%f');
+      install -Dm644 target/service-olog-4.7.3.jar $out/share/java
+      # Strip the script at the beginning of the jar, so that we are able to
+      # canonicalize it
+      sed -i '1,/^exit 0$/d' $out/share/java/$jarName
+
+      makeWrapper ${jdk}/bin/java $out/bin/${final.pname} \
+        --add-flags "-classpath ''${classpath#:}" \
+        --add-flags "-jar $out/share/java/$jarName"
+
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Online logbook for experimental and industrial logging";
+      homepage = "https://olog.readthedocs.io/en/latest/";
+      license = lib.licenses.epl10;
+      maintainers = with epnixLib.maintainers; [minijackson];
+      inherit (jdk.meta) platforms;
+    };
+  })
