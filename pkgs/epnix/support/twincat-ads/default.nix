a53bd60ed37e54acce3e0672024fd15cf506f9c0
Alexis Gaget <alexis.gaget@cea.fr>
2023-12-20 17:07:11 +0100

    support/twincat-ads: add ESS ADS driver support (Beckhoff TwinCAT)
    (cherry picked from commit abc9a0abb0f4b2cbdc2b72dab1471b3de1c79ccf)

---
+++ pkgs/epnix/support/twincat-ads/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  epnixLib,
+  epnix,
+  mkEpicsPackage,
+  fetchFromGitLab,
+  local_config_site ? {},
+  local_release ? {},
+}:
+mkEpicsPackage rec {
+  pname = "twincat-ads";
+  version = "2024.01.11";
+  varname = "TWINCATADS";
+
+  inherit local_config_site local_release;
+
+  src = fetchFromGitLab {
+    domain = "gitlab.esss.lu.se";
+    owner = "epics-modules";
+    repo = "epics-twincat-ads";
+    rev = "c8e8b52c1f34640eca97b8fb053e793dc68acc0a";
+    fetchSubmodules = true;
+    sha256 = "sha256-f7hod1N1AzCh+W7nHl9VCA+nuwpJAboSh19Dq80n/2E=";
+  };
+
+  propagatedBuildInputs = with epnix.support; [asyn calc];
+
+  preBuild = ''
+    touch configure/RELEASE_PATHS.local
+    touch configure/RELEASE_LIBS.local
+  '';
+
+  meta = {
+    description = "Module providing EPICS support for ADS Protocol (Automation Device Specification)";
+    homepage = "https://gitlab.esss.lu.se/epics-modules/epics-twincat-ads";
+    # Wait for ESS team answer about the license :  https://gitlab.esss.lu.se/epics-modules/epics-twincat-ads/-/issues/1
+    license = lib.licenses.free;
+    maintainers = with epnixLib.maintainers; [agaget];
+  };
+}
