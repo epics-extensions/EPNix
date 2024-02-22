aefd9572f6a717396a91ed631cfcb821f5a7c6e2
Minijackson <minijackson@riseup.net>
2024-02-22 09:26:44 +0100

    pkgs/lewis: init at 1.3.1

---
+++ pkgs/epnix/tools/lewis/default.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  approvaltests,
+  setuptools,
+  wheel,
+  json-rpc,
+  mock,
+  pytest,
+  pyyaml,
+  pyzmq,
+  scanf,
+  semantic-version,
+}:
+buildPythonPackage rec {
+  pname = "lewis";
+  version = "1.3.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ess-dmsc";
+    repo = "lewis";
+    rev = "v${version}";
+    hash = "sha256-7iMREHt6W26IzCFsRmojHqGuqIUHaCuvsKMMHuYflz0=";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+    wheel
+  ];
+
+  propagatedBuildInputs = [
+    json-rpc
+    pyyaml
+    pyzmq
+    scanf
+    semantic-version
+  ];
+
+  checkInputs = [
+    approvaltests
+    mock
+    pytest
+  ];
+
+  pythonImportsCheck = ["lewis"];
+
+  meta = with lib; {
+    description = "Let's write intricate simulators";
+    homepage = "https://github.com/ess-dmsc/lewis";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [minijackson];
+  };
+}
