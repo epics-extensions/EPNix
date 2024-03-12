db030c1b1976c5fa427529efb83225efecb8587c
Minijackson <minijackson@riseup.net>
2024-03-12 08:52:32 +0100

    pkgs/psu-simulator: migrate to lewis

--- pkgs/doc-support/psu-simulator/default.nix
+++ pkgs/doc-support/psu-simulator/default.nix
@@ -1,13 +1,20 @@
 {
-  poetry2nix,
   lib,
+  epnix,
   epnixLib,
 }:
-poetry2nix.mkPoetryApplication {
-  projectDir = ./.;
+(epnix.mkLewisSimulator {
+  name = "psu_simulator";
+  package = "psu_simulator";
+  source = ./.;
+})
+// {
+  pname = "psu_simulator";
+  version = "0.2.0";
 
   meta = {
-    homepage = "https://epics-extensions.github.io/EPNix/";
+    description = "A power supply simulator for the StreamDevice tutorial";
+    homepage = "https://epics-extensions.github.io/EPNix/ioc/tutorials/streamdevice.html";
     license = lib.licenses.asl20;
     maintainers = with epnixLib.maintainers; [minijackson];
   };
