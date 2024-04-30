dd308539ea3eb75469560f131ba8ccd32363f694
Patrick Hilhorst <philhorst@highvolteng.com>
2024-04-30 07:29:18 +0000

    phoebus-scan-server: nixos module
    Closes #74

--- nixos/module-list.nix
+++ nixos/module-list.nix
@@ -3,6 +3,7 @@
   ./modules/ca-gateway.nix
   ./modules/phoebus/alarm-logger.nix
   ./modules/phoebus/alarm-server.nix
+  ./modules/phoebus/scan-server.nix
   ./modules/phoebus/olog.nix
   ./modules/phoebus/save-and-restore.nix
 ]
