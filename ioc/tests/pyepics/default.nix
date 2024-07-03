b79cb16fd794a8a0495d1acdb773371bf1b9ba9c
Minijackson <minijackson@riseup.net>
2024-07-03 16:06:37 +0200

    ioc/tests/pyepics: init simple test
    (cherry picked from commit 3e53f2e2701df99ce7c08177ba1723ad9d81e41c)

---
+++ ioc/tests/pyepics/default.nix
@@ -0,0 +1,47 @@
+{pkgs, ...}: let
+  inherit (pkgs) epnixLib lib;
+in
+  pkgs.nixosTest {
+    name = "pytest";
+    meta.maintainers = with epnixLib.maintainers; [minijackson];
+
+    extraPythonPackages = p: [p.pyepics];
+    skipTypeCheck = true;
+
+    nodes.ioc = {
+      imports = [
+        (epnixLib.testing.softIoc ''
+          record(ai, "AI") { }
+          record(stringout, "STRINGOUT") { }
+        '')
+      ];
+    };
+
+    testScript = let
+      python = lib.getExe (pkgs.python3.withPackages (p: [p.pyepics]));
+      iocTestScript = pkgs.writeText "iocTestScript.py" ''
+        import os
+
+        import epics
+
+        os.environ["EPICS_CA_AUTO_ADDR_LIST"] = "NO"
+        os.environ["EPICS_CA_ADDR_LIST"] = "localhost"
+
+        stringout = epics.PV("STRINGOUT")
+
+        assert epics.caget("AI") == 0
+        assert stringout.get() == ""
+
+        assert epics.caput("AI", 42.0, wait=True) == 1
+        assert stringout.put("hello", wait=True) == 1
+
+        assert epics.caget("AI") == 42
+        assert stringout.get() == "hello"
+      '';
+    in ''
+      start_all()
+      ioc.wait_for_unit("ioc.service")
+
+      print(ioc.succeed("${python} ${iocTestScript}"))
+    '';
+  }
