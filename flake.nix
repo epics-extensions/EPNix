b7279c1754e7744fce482ad1ca66ec6ed09978bf
Minijackson <minijackson@riseup.net>
2024-07-08 14:28:53 +0200

    pkgs/tests: init for testing build of unexposed packages
    (cherry picked from commit 34d14bdca2e0b6d85f3ff80370002297d2541401)

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
             constituents = builtins.attrValues self.packages.${system};
           };
         }
+        // (import ./pkgs/tests {inherit pkgs self;})
         // (import ./ioc/tests {inherit pkgs self;})
         // (import ./nixos/tests/all-tests.nix {inherit nixpkgs pkgs self system;});
 
