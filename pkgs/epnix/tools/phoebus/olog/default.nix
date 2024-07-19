2c1c9455f94d84b63cd2e4ed5193ed5380247eff
Loïc <loic.caouen@gmail.com>
2024-07-19 09:09:06 +0200

    pkgs/phoebus-olog: 4.7.3 -> 4.7.7
    removed sed because jar file was invalid
    
    (cherry picked from commit 369dde7ac8296533ddbd6ebd7d05b1103a331a85)

--- pkgs/epnix/tools/phoebus/olog/default.nix
+++ pkgs/epnix/tools/phoebus/olog/default.nix
@@ -8,16 +8,16 @@
 }:
 maven.buildMavenPackage rec {
   pname = "phoebus-olog";
-  version = "4.7.3";
+  version = "4.7.7";
 
   src = fetchFromGitHub {
     owner = "Olog";
     repo = "phoebus-olog";
     rev = "v${version}";
-    hash = "sha256-WwRB4QtZBeH6GptTZJ02CBpP7BGzjZbwMYQrOmGevFo=";
+    hash = "sha256-AHZowe4mmBpiFd5MMVRrnUHeTOJDwE6f0sZFUF+07lo=";
   };
 
-  mvnHash = "sha256-D1n5PfGulIgdjd60mChVLH1kQDOUcc/TvEw3oJUZ1h4=";
+  mvnHash = "sha256-xaoaoL1a9VP7e4HdI2YuOUPOADnMYlPesVbkhgLz3+M=";
   mvnParameters = "-Dmaven.javadoc.skip=true -Dmaven.source.skip=true -Pdeployable-jar -Dproject.build.outputTimestamp=1980-01-01T00:00:02Z";
 
   nativeBuildInputs = [makeWrapper];
@@ -30,10 +30,7 @@ maven.buildMavenPackage rec {
 
     jarName="service-olog-${version}.jar"
 
-    install -Dm644 target/service-olog-4.7.3.jar $out/share/java
-    # Strip the script at the beginning of the jar, so that we are able to
-    # canonicalize it
-    sed -i '1,/^exit 0$/d' $out/share/java/$jarName
+    install -Dm644 target/service-olog-${version}.jar $out/share/java
 
     makeWrapper ${lib.getExe jre} $out/bin/${meta.mainProgram} \
       --add-flags "-jar $out/share/java/$jarName"
