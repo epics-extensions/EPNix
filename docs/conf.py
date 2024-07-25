2daf4324e1268a8129bb6c8b355b1256c8fdbf52
Minijackson <minijackson@riseup.net>
2024-07-25 10:16:10 +0200

    docs: move vale over to new directory, configure for RST

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,12 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "_vale",
+]
 
 # numfig = True
 
