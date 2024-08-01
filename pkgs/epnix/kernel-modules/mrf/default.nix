3dbad097e0893ca02b9323e239b906fcdd05fdb2
Minijackson <minijackson@riseup.net>
2024-08-01 13:54:02 +0200

    pkgs.mrf-driver: fix build with Linux >= 6.3

--- pkgs/epnix/kernel-modules/mrf/default.nix
+++ pkgs/epnix/kernel-modules/mrf/default.nix
@@ -1,13 +1,22 @@
 {
   stdenv,
-  lib,
   kernel,
   epnix,
+  fetchpatch,
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "mrf-driver";
 
   inherit (epnix.support.mrfioc2) version src;
+
+  patches = [
+    (fetchpatch {
+      name = "use-vm_flags_set-on-kernels-6-3.patch";
+      url = "https://github.com/epics-modules/mrfioc2/commit/64634a23a035d336de5d720b2e1ecafcc918d737.patch";
+      hash = "sha256-6/obmLfLKfhcwn12O0KKpogWCzg7Evs1kIoNm0x7Puw=";
+      stripLen = 2;
+    })
+  ];
 
   # Needed for kernel modules
   hardeningDisable = ["format" "pic"];
