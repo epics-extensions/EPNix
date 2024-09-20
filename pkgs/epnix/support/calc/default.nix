d9ca2b6f912fa01777a2ce2d520587f8e67377dc
Minijackson <minijackson@riseup.net>
2024-09-19 22:22:22 -0400

    support.calc: reorder attributes
    (cherry picked from commit 72f24b4992b4e38089ced1081957379025f5e256)

--- pkgs/epnix/support/calc/default.nix
+++ pkgs/epnix/support/calc/default.nix
@@ -11,16 +11,16 @@ mkEpicsPackage rec {
   version = "3-7-5";
   varname = "CALC";
 
-  inherit local_config_site local_release;
-
-  buildInputs = with epnix.support; [sscan];
-
   src = fetchFromGitHub {
     owner = "epics-modules";
     repo = "calc";
     rev = "R${version}";
     sha256 = "sha256-S40HtO7HXDS27u7wmlxuo7oV1abtj1EaXfIz0Kj1IM0=";
   };
+
+  buildInputs = with epnix.support; [sscan];
+
+  inherit local_config_site local_release;
 
   meta = {
     description = "Support for run-time expression evaluation";
