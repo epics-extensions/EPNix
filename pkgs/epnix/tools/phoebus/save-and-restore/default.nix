c508744179fea2a948a341a8eae09b2c7a360e91
Minijackson <minijackson@riseup.net>
2024-12-09 13:39:23 +0100

    phoebus-save-and-restore: use phoebus-setup-hook' JDK

--- pkgs/epnix/tools/phoebus/save-and-restore/default.nix
+++ pkgs/epnix/tools/phoebus/save-and-restore/default.nix
@@ -5,7 +5,6 @@
   maven,
   makeWrapper,
   epnix,
-  jdk,
 }: let
   buildDate = "2022-02-24T07:56:00Z";
 in
@@ -53,6 +52,6 @@ in
       mainProgram = "phoebus-save-and-restore";
       license = lib.licenses.epl10;
       maintainers = with epnixLib.maintainers; [minijackson];
-      inherit (jdk.meta) platforms;
+      inherit (epnix.phoebus-setup-hook.meta) platforms;
     };
   }
