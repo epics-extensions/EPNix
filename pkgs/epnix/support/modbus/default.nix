4ff8127638eb3ec7e76a9b6bdda8a80fd4936248
Minijackson <minijackson@riseup.net>
2024-12-12 11:38:52 +0100

    support/modbus: 3-2 -> 3-4
    and document the upstream change in the modbus.dbd -> modbusApp.dbd
    
    release notes:
    
    - https://github.com/epics-modules/modbus/blob/master/RELEASE.md#r3-3-december-7-2023
    - https://github.com/epics-modules/modbus/blob/master/RELEASE.md#r3-4-december-1-2024

--- pkgs/epnix/support/modbus/default.nix
+++ pkgs/epnix/support/modbus/default.nix
@@ -7,14 +7,14 @@
 }:
 mkEpicsPackage rec {
   pname = "modbus";
-  version = "3-2";
+  version = "3-4";
   varname = "MODBUS";
 
   src = fetchFromGitHub {
     owner = "epics-modules";
     repo = pname;
-    rev = "R3-2";
-    hash = "sha256-k8MSgNxib4JT0JTbs0BOm75HIVvxHuVPPlo7VcMCnzg=";
+    rev = "R${version}";
+    hash = "sha256-0v6eLWdjgYKbFOHWaW1NSfN/gG5XHVRD9jan55dXWW0=";
   };
 
   propagatedBuildInputs = with epnix.support; [asyn];
