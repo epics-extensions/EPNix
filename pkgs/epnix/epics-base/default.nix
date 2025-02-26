d49013f2f05f88c0fb38a2060fe5374311983acc
Minijackson <minijackson@riseup.net>
2025-02-26 09:05:49 +0100

    epics-base: 7.0.8.1 -> 7.0.9
    re-enabled the fortify3 compilation option,
    since it was fixed upstream

--- pkgs/epnix/epics-base/default.nix
+++ pkgs/epnix/epics-base/default.nix
@@ -5,7 +5,6 @@
   buildPackages,
   mkEpicsPackage,
   fetchgit,
-  fetchpatch,
   version,
   hash,
   local_config_site ? {},
@@ -13,7 +12,6 @@
 }:
 with lib; let
   older = versionOlder version;
-  atLeast = versionAtLeast version;
 
   generateConf = (epnixLib.formats.make {}).generate;
 
@@ -135,16 +133,6 @@ in
 
     # TODO: Some tests fail
     doCheck = false;
-
-    # _FORTIFY_SOURCE=3 detects a false-positive buffer overflow in some cases:
-    #     *** buffer overflow detected ***: terminated
-    #
-    # EPICS automatically falls back to _FORTIFY_SOURCE=2 since 7.0.8.1, but this doesn't work in
-    # the nix build.
-    # Being tracked in https://github.com/epics-base/epics-base/issues/514, hopefully with a fix
-    # in EPICS 7.0.9
-
-    hardeningDisable = ["fortify3"];
 
     meta = {
       description = "The Experimental Physics and Industrial Control System";
