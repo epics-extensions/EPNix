a4eef00c4a79b58915722fdfe5b6e9d2734140f6
Minijackson <minijackson@riseup.net>
2025-04-25 14:42:14 +0200

    pkgs: implement by-name mechanism and scope for main packages
    use scope so that packages don't have to specify "epnix" as package
    argument, and can directly mention the dependency

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,16 @@
 epnixLib: inputs: final: prev: let
   inherit (final) callPackage;
   # From prev, else it somehow causes an infinite recursion
-  inherit (prev) recurseIntoAttrs;
-  recurseExtensible = f: recurseIntoAttrs (final.lib.makeExtensible f);
+  inherit (prev) recurseIntoAttrs lib;
+  scope = prev: f: recurseIntoAttrs (lib.makeScope prev.newScope f);
+
+  importByName = baseDirectory: self:
+    lib.pipe baseDirectory [
+      builtins.readDir
+      (lib.filterAttrs (_: type: type == "directory"))
+      # Call the packages
+      (lib.mapAttrs (name: _type: self.callPackage (baseDirectory + "/${name}/package.nix") {}))
+    ];
 in
   recurseIntoAttrs {
     inherit epnixLib;
@@ -36,99 +44,103 @@ in
           });
       };
 
-    epnix = recurseExtensible (self: {
-      # EPICS base
+    epnix = scope prev (self: let
+      epnixScope = self;
+    in
+      {
+        # EPICS base
 
-      epics-base7 = callPackage ./epnix/epics-base {
-        version = "7.0.9";
-        hash = "sha256-RPlJhh7ORobYlM7gq6uDZkrO5z579Q7hyLEQ1xiHvFY=";
-      };
-      epics-base3 = callPackage ./epnix/epics-base {
-        version = "3.15.9";
-        hash = "sha256-QWScmCEaG0F6OW6LPCaFur4W57oRl822p7wpzbYhOuA=";
-      };
-      epics-base = self.epics-base7;
+        epics-base7 = callPackage ./epnix/epics-base {
+          version = "7.0.9";
+          hash = "sha256-RPlJhh7ORobYlM7gq6uDZkrO5z579Q7hyLEQ1xiHvFY=";
+        };
+        epics-base3 = callPackage ./epnix/epics-base {
+          version = "3.15.9";
+          hash = "sha256-QWScmCEaG0F6OW6LPCaFur4W57oRl822p7wpzbYhOuA=";
+        };
+        epics-base = self.epics-base7;
 
-      epicsSetupHook = callPackage ./build-support/epics-setup-hook {};
+        epicsSetupHook = callPackage ./build-support/epics-setup-hook {};
 
-      # EPICS support modules
+        # EPICS support modules
 
-      support = recurseExtensible (_self: {
-        adsDriver = callPackage ./epnix/support/adsDriver {};
-        asyn = callPackage ./epnix/support/asyn {};
-        autoparamDriver = callPackage ./epnix/support/autoparamDriver {};
-        autosave = callPackage ./epnix/support/autosave {};
-        busy = callPackage ./epnix/support/busy {};
-        calc = callPackage ./epnix/support/calc {};
-        devlib2 = callPackage ./epnix/support/devlib2 {};
-        epics-systemd = callPackage ./epnix/support/epics-systemd {};
-        gtest = callPackage ./epnix/support/gtest {};
-        ipac = callPackage ./epnix/support/ipac {};
-        modbus = callPackage ./epnix/support/modbus {};
-        mrfioc2 = callPackage ./epnix/support/mrfioc2 {};
-        opcua = callPackage ./epnix/support/opcua {open62541 = self.open62541_1_3;};
-        pvxs = callPackage ./epnix/support/pvxs {};
-        reccaster = callPackage ./epnix/support/reccaster {};
-        seq = callPackage ./epnix/support/seq {};
-        snmp = callPackage ./epnix/support/snmp {};
-        sscan = callPackage ./epnix/support/sscan {};
-        StreamDevice = callPackage ./epnix/support/StreamDevice {};
-        twincat-ads = callPackage ./epnix/support/twincat-ads {};
-      });
+        support = scope self (self: {
+          adsDriver = callPackage ./epnix/support/adsDriver {};
+          asyn = callPackage ./epnix/support/asyn {};
+          autoparamDriver = callPackage ./epnix/support/autoparamDriver {};
+          autosave = callPackage ./epnix/support/autosave {};
+          busy = callPackage ./epnix/support/busy {};
+          calc = callPackage ./epnix/support/calc {};
+          devlib2 = callPackage ./epnix/support/devlib2 {};
+          epics-systemd = callPackage ./epnix/support/epics-systemd {};
+          gtest = callPackage ./epnix/support/gtest {};
+          ipac = callPackage ./epnix/support/ipac {};
+          modbus = callPackage ./epnix/support/modbus {};
+          mrfioc2 = callPackage ./epnix/support/mrfioc2 {};
+          opcua = callPackage ./epnix/support/opcua {open62541 = epnixScope.open62541_1_3;};
+          pvxs = callPackage ./epnix/support/pvxs {};
+          reccaster = callPackage ./epnix/support/reccaster {};
+          seq = callPackage ./epnix/support/seq {};
+          snmp = callPackage ./epnix/support/snmp {};
+          sscan = callPackage ./epnix/support/sscan {};
+          StreamDevice = callPackage ./epnix/support/StreamDevice {};
+          twincat-ads = callPackage ./epnix/support/twincat-ads {};
+        });
 
-      # EPICS related tools and extensions
+        # EPICS related tools and extensions
 
-      archiver-appliance = callPackage ./epnix/tools/archiver-appliance {
-        jdk = prev.jdk17;
-      };
+        archiver-appliance = callPackage ./epnix/tools/archiver-appliance {
+          jdk = prev.jdk17;
+        };
 
-      ca-gateway = callPackage ./epnix/tools/ca-gateway {};
+        ca-gateway = callPackage ./epnix/tools/ca-gateway {};
 
-      channel-finder-service = callPackage ./epnix/tools/channel-finder/service {};
+        channel-finder-service = callPackage ./epnix/tools/channel-finder/service {};
 
-      # Lewis needs Python < 3.12
-      inherit (final.python311Packages) lewis;
+        # Lewis needs Python < 3.12
+        inherit (final.python311Packages) lewis;
 
-      inherit (final.python3Packages) pyepics;
+        inherit (final.python3Packages) pyepics;
 
-      pythonSoftIOC = final.python3Packages.softioc;
+        pythonSoftIOC = final.python3Packages.softioc;
 
-      inherit (callPackage ./epnix/tools/lewis/lib.nix {}) mkLewisSimulator;
+        inherit (callPackage ./epnix/tools/lewis/lib.nix {}) mkLewisSimulator;
 
-      pcas = callPackage ./epnix/tools/pcas {};
+        pcas = callPackage ./epnix/tools/pcas {};
 
-      phoebus = callPackage ./epnix/tools/phoebus/client {};
-      phoebus-unwrapped = callPackage ./epnix/tools/phoebus/client-unwrapped {
-        jdk = prev.jdk21;
-        openjfx = prev.openjfx21;
-      };
-      phoebus-alarm-server = callPackage ./epnix/tools/phoebus/alarm-server {};
-      phoebus-alarm-logger = callPackage ./epnix/tools/phoebus/alarm-logger {};
-      phoebus-archive-engine = callPackage ./epnix/tools/phoebus/archive-engine {};
-      phoebus-deps = callPackage ./epnix/tools/phoebus/deps {jdk = prev.jdk21;};
-      phoebus-olog = callPackage ./epnix/tools/phoebus/olog {jdk = prev.jdk21;};
-      phoebus-pva = callPackage ./epnix/tools/phoebus/pva {};
-      phoebus-save-and-restore = callPackage ./epnix/tools/phoebus/save-and-restore {};
-      phoebus-scan-server = callPackage ./epnix/tools/phoebus/scan-server {};
-      phoebus-setup-hook = callPackage ./epnix/tools/phoebus/setup-hook {jdk = prev.jdk21_headless;};
+        phoebus = callPackage ./epnix/tools/phoebus/client {};
+        phoebus-unwrapped = callPackage ./epnix/tools/phoebus/client-unwrapped {
+          jdk = prev.jdk21;
+          openjfx = prev.openjfx21;
+        };
+        phoebus-alarm-server = callPackage ./epnix/tools/phoebus/alarm-server {};
+        phoebus-alarm-logger = callPackage ./epnix/tools/phoebus/alarm-logger {};
+        phoebus-archive-engine = callPackage ./epnix/tools/phoebus/archive-engine {};
+        phoebus-deps = callPackage ./epnix/tools/phoebus/deps {jdk = prev.jdk21;};
+        phoebus-olog = callPackage ./epnix/tools/phoebus/olog {jdk = prev.jdk21;};
+        phoebus-pva = callPackage ./epnix/tools/phoebus/pva {};
+        phoebus-save-and-restore = callPackage ./epnix/tools/phoebus/save-and-restore {};
+        phoebus-scan-server = callPackage ./epnix/tools/phoebus/scan-server {};
+        phoebus-setup-hook = callPackage ./epnix/tools/phoebus/setup-hook {jdk = prev.jdk21_headless;};
 
-      procServ = callPackage ./epnix/tools/procServ {};
+        procServ = callPackage ./epnix/tools/procServ {};
 
-      # Other utilities
+        # Other utilities
 
-      # Needed by support/opcua
-      open62541_1_3 = callPackage ./epnix/tools/open62541_1_3 {};
+        # Needed by support/opcua
+        open62541_1_3 = callPackage ./epnix/tools/open62541_1_3 {};
 
-      # EPNix specific packages
-      docs = callPackage ./docs {
-        nixdomainLib = inputs.sphinxcontrib-nixdomain.lib;
-      };
+        # EPNix specific packages
+        docs = callPackage ./docs {
+          nixdomainLib = inputs.sphinxcontrib-nixdomain.lib;
+        };
 
-      # Documentation support packages
-      psu-simulator = callPackage ./doc-support/psu-simulator {};
+        # Documentation support packages
+        psu-simulator = callPackage ./doc-support/psu-simulator {};
 
-      ci-scripts = recurseExtensible (_self: {
-        build-docs-multiversion = callPackage ./ci-scripts/build-docs-multiversion.nix {};
-      });
-    });
+        ci-scripts = scope self (_self: {
+          build-docs-multiversion = callPackage ./ci-scripts/build-docs-multiversion.nix {};
+        });
+      }
+      // (importByName ./by-name self));
   }
