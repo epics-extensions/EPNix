8a18680254568199cab2d5cc65d78533a24ee13c
Minijackson <minijackson@riseup.net>
2025-04-25 15:01:18 +0200

    ca-gateway: move to by-name/

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -93,8 +93,6 @@ in
           jdk = prev.jdk17;
         };
 
-        ca-gateway = callPackage ./epnix/tools/ca-gateway {};
-
         channel-finder-service = callPackage ./epnix/tools/channel-finder/service {};
 
         # Lewis needs Python < 3.12
