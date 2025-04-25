bb099c13abe8566312facf901b76d729ed058b6b
Minijackson <minijackson@riseup.net>
2025-04-25 20:57:52 +0200

    pyepics: move to by-name/

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -23,7 +23,6 @@ in
       ++ [
         (final: _prev:
           {
-            pyepics = final.callPackage ./epnix/python-modules/pyepics {};
             recceiver = final.callPackage ./epnix/tools/channel-finder/recceiver {};
             scanf = final.callPackage ./epnix/tools/scanf {};
             epicscorelibs = final.callPackage ./epnix/python-modules/epicscorelibs {};
