d0b6a83b9de5ae35a71dd6fcae0e59e528795e42
Minijackson <minijackson@riseup.net>
2025-04-25 18:26:53 +0200

    phoebus-alarm-server: move to by-name/

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -107,7 +107,6 @@ in
           jdk = prev.jdk21;
           openjfx = prev.openjfx21;
         };
-        phoebus-alarm-server = callPackage ./epnix/tools/phoebus/alarm-server {};
         phoebus-alarm-logger = callPackage ./epnix/tools/phoebus/alarm-logger {};
         phoebus-archive-engine = callPackage ./epnix/tools/phoebus/archive-engine {};
         phoebus-olog = callPackage ./epnix/tools/phoebus/olog {jdk = prev.jdk21;};
