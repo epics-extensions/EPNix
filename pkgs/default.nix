214d30f3dab59433f8b3cd48cf8ad8a1fe937733
Minijackson <minijackson@riseup.net>
2025-04-25 18:34:46 +0200

    phoebus-olog: move to by-name/

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -107,7 +107,6 @@ in
           jdk = prev.jdk21;
           openjfx = prev.openjfx21;
         };
-        phoebus-olog = callPackage ./epnix/tools/phoebus/olog {jdk = prev.jdk21;};
         phoebus-pva = callPackage ./epnix/tools/phoebus/pva {};
         phoebus-save-and-restore = callPackage ./epnix/tools/phoebus/save-and-restore {};
         phoebus-scan-server = callPackage ./epnix/tools/phoebus/scan-server {};
