b496ff9e0be7339916d79244738534d84288ecc0
Minijackson <minijackson@riseup.net>
2025-06-05 13:44:34 +0200

    pkgs/phoebus: add support for specifying a settings file

--- pkgs/by-name/phoebus/package.nix
+++ pkgs/by-name/phoebus/package.nix
@@ -5,6 +5,7 @@
   makeWrapper,
   makeDesktopItem,
   copyDesktopItems,
+  settingsFile ? null,
   # Inspired by:
   # https://epics.anl.gov/tech-talk/2024/msg00895.php
   java_opts ? "-XX:MinHeapSize=128m -XX:MaxHeapSize=4g -XX:InitialHeapSize=1g -XX:MaxHeapFreeRatio=10 -XX:MinHeapFreeRatio=5 -XX:-ShrinkHeapInSteps -XX:NativeMemoryTracking=detail",
@@ -23,6 +24,7 @@ stdenv.mkDerivation {
 
     # This wrapper for the `phoebus-unwrapped` executable sets the `JAVA_OPTS`
     makeWrapper "${lib.getExe phoebus-unwrapped}" "$out/bin/$pname" \
+      ${lib.optionalString (settingsFile != null) ''--add-flags "-settings ${settingsFile}"''} \
       --prefix JAVA_OPTS " " "${java_opts}"
 
     runHook postInstall
