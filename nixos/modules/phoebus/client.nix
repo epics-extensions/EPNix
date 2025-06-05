1ac40febe32bfa32f9018b93c7bce92df126d226
Minijackson <minijackson@riseup.net>
2025-06-05 13:45:12 +0200

    nixos/phoebus-client: add settings, settingsFile options
    this enables users to specify the Phoebus client settings.ini file,
    directly in the NixOS configuration

--- nixos/modules/phoebus/client.nix
+++ nixos/modules/phoebus/client.nix
@@ -5,10 +5,55 @@
   ...
 }: let
   cfg = config.programs.phoebus-client;
-  pkg = pkgs.epnix.phoebus.override {java_opts = cfg.java_opts;};
+  settingsFormat = pkgs.formats.javaProperties {};
+  pkg = pkgs.epnix.phoebus.override {
+    inherit (cfg) settingsFile java_opts;
+  };
 in {
   options.programs.phoebus-client = {
     enable = lib.mkEnableOption "installing and configuring the Phoebus client";
+
+    settings = lib.mkOption {
+      description = ''
+        Phoebus preference setting,
+        as defined in the [Preferences Listing] Phoebus documentation.
+
+          [Preferences Listing]: https://control-system-studio.readthedocs.io/en/latest/preference_properties.html
+      '';
+      default = {};
+      example = {
+        "org.phoebus.applications.alarm/server" = "localhost:9092";
+        "org.phoebus.applications.alarm/config_names" = "Accelerator, Demo";
+        "org.csstudio.trends.databrowser3/urls" = "pbraw://localhost:8080/retrieval";
+      };
+      type = lib.types.submodule {
+        freeformType = settingsFormat.type;
+        options = {
+        };
+      };
+    };
+
+    settingsFile = lib.mkOption {
+      description = ''
+        Path to a {file}`settings.ini` file, to be used by Phoebus.
+
+        By default,
+        the file is generated from the {nix:option}`settings` option.
+
+        :::{note}
+        Use the {nix:option}`settingsFile` option
+        if you want to import an already existing {file}`settings.ini` file.
+        If you override this option, {nix:option}`settings` options are ignored.
+        :::
+
+        May also point to a remote URL.
+      '';
+      type = with lib.types; either path str;
+      default = settingsFormat.generate "phoebus-settings.ini" cfg.settings;
+      defaultText = "<file generated from the 'settings' options>";
+      example = lib.literalExpression "./settings.ini";
+    };
+
     java_opts = lib.mkOption {
       type = lib.types.str;
       default = "-XX:MinHeapSize=128m -XX:MaxHeapSize=4g -XX:InitialHeapSize=1g -XX:MaxHeapFreeRatio=10 -XX:MinHeapFreeRatio=5 -XX:-ShrinkHeapInSteps -XX:NativeMemoryTracking=detail";
