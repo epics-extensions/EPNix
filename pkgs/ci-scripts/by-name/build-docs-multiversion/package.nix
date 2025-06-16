b393646fa006711380bcb90c42fe8070d751a339
Minijackson <minijackson@riseup.net>
2025-06-16 15:07:14 +0200

    build-docs-multiversion: fetch EPNix versions by itself
    The CI script run by Nix now downloads the EPNix archives by itself,
    instead of requiring the GitHub workflow to do it.
    
    This simplifies the workflow, and enables us to just run:
    
        nix run .#ci-scripts/build-docs-multiversion
    
    Which will do the whole EPNix website build.
    
    This is also one less place to edit for new EPNix releases.

--- pkgs/ci-scripts/by-name/build-docs-multiversion/package.nix
+++ pkgs/ci-scripts/by-name/build-docs-multiversion/package.nix
@@ -4,9 +4,13 @@
   writeText,
   writers,
   epnixLib,
+  curl,
+  gnutar,
+  gzip,
 }: let
   inherit (epnixLib.versions) stable;
   versions = epnixLib.versions.all;
+  version-branches = epnixLib.versions.all-branches;
   baseurl = "https://epics-extensions.github.io/EPNix";
   # Make a redirection using the <meta> tag,
   # with a delay of 1 second, because Google considers them not permanent
@@ -16,6 +20,8 @@
   redirect = writeText "stable-redirect.html" ''
     <meta http-equiv=refresh content="1;url=${stable}/">
   '';
+
+  source-repository = "https://github.com/epics-extensions/EPNix";
 
   # Put the stable version first
   versionsbyPriority = [stable] ++ (lib.remove stable versions);
@@ -56,18 +62,36 @@ in
   writeShellApplication {
     name = "build-docs-multiversion";
 
+    runtimeInputs = [curl gnutar gzip];
+
     text = ''
+      mkdir -p outputs/out
+      out="$(pwd)/outputs/out"
+      cd "$(mktemp -d)"
+
+      # Download every specified branch of EPNix
+      for version in ${toString version-branches}; do
+        curl -LSf "${source-repository}/archive/refs/heads/$version.tar.gz" | tar xz
+        mv "EPNix-$version" "$version"
+      done
+
+      # The "master" branch is called "dev"
+      mv master dev
+
+      # Build the documentation
       for version in ${toString versions}; do
         mkdir -p "./book/$version"
-        cp ${versions_json} "./$version/docs/versions.json"
-        git -C "$version" add docs/versions.json
+        [[ -d "$version/docs" ]] && cp ${versions_json} "./$version/docs/versions.json"
         nix build "./$version#docs" --print-build-logs
         cp -LrT --no-preserve=mode,ownership ./result/share/doc/epnix/html "./book/$version"
       done
+
       cp ${redirect} ./book/index.html
       cp ${sitemap} ./book/sitemap.xml
       cp ${robots} ./book/robots.txt
       echo "google-site-verification: googlec2385d4b797d68b3.html" > ./book/googlec2385d4b797d68b3.html
+
+      cp -avfT book "$out/book"
     '';
 
     meta = {
