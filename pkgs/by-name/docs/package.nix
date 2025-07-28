87641c5e6ad6a5725456c8984ce8cf2e3504017d
Minijackson <minijackson@riseup.net>
2025-07-28 16:59:47 +0200

    docs: copy all generated files when entering devshell

--- pkgs/by-name/docs/package.nix
+++ pkgs/by-name/docs/package.nix
@@ -155,20 +155,17 @@ stdenvNoCC.mkDerivation {
   dontConfigure = true;
 
   postPatch = ''
-    mkdir -p ioc/references
-    mkdir -p pkgs
-
-    cp -fv "${nixosOptionsSpec}" nixos-options.json
-    cp -fv "${writeText "ioc-options.md" iocOptionsPandoc}" ioc/references/options.md
-    cp -fv "${writeText "ioc-packages.md" iocPkgsListPandoc}" ioc/references/packages.md
-    cp -fv "${writeText "packages.md" pkgsListPandoc}" pkgs/packages.md
+    install -Dv "${nixosOptionsSpec}" nixos-options.json
+    install -Dv "${writeText "ioc-options.md" iocOptionsPandoc}" ioc/references/options.md
+    install -Dv "${writeText "ioc-packages.md" iocPkgsListPandoc}" ioc/references/packages.md
+    install -Dv "${writeText "packages.md" pkgsListPandoc}" pkgs/packages.md
   '';
 
   shellHook = ''
     if [[ -f docs/conf.py ]]; then
-      install -v "${nixosOptionsSpec}" docs/nixos-options.json
+      (cd docs; runHook postPatch)
     elif [[ -f conf.py ]]; then
-      install -v "${nixosOptionsSpec}" nixos-options.json
+      runHook postPatch
     else
       echo "Couldn't find root of docs directory, not copying options.json files"
     fi
