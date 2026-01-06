25b31b048f875905b7e37861d9e97e23b7748a28
Minijackson <minijackson@riseup.net>
2026-01-06 14:17:13 +0100

    flake: expose outside scope in the flake's packages output
    and remove all pkgs tests,
    since they are now in the "allPackages" test

--- pkgs/tests/default.nix
+++ pkgs/tests/default.nix
@@ -1,13 +1,4 @@
-# For packages that are not directly exposed to the user,
-# but should still work.
-#
-# For example kernel modules, which depend on the kernel version,
-# or Python libraries, which depend on the Python version.
+# Non-NixOS tests
 { pkgs, ... }:
 {
-  aiaoca-default-python = pkgs.python3Packages.aioca;
-  channelfinder-default-python = pkgs.python3Packages.channelfinder;
-  mrf-driver-default-linux = pkgs.linuxPackages.mrf;
-  pvapy-default-python = pkgs.python3Packages.pvapy;
-  recceiver-default-python = pkgs.python311Packages.recceiver;
 }
