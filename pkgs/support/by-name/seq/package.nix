02fc06085fd7bad5d1edbfd0fbc7a4c62026baa6
Minijackson <minijackson@riseup.net>
2026-07-24 11:07:45 +0200

    support.seq: fix C23 compilation

--- pkgs/support/by-name/seq/package.nix
+++ pkgs/support/by-name/seq/package.nix
@@ -2,6 +2,7 @@
   epnixLib,
   mkEpicsPackage,
   fetchdarcs,
+  fetchpatch2,
   re2c,
 }:
 mkEpicsPackage {
@@ -15,6 +16,12 @@ mkEpicsPackage {
     ./remove-date.patch
     # See: https://epics.anl.gov/epics/tech-talk/2022/msg01183.php
     ./remove_rules_compat.patch
+
+    (fetchpatch2 {
+      name = "lemon-add-missing-arguments-to-prototypes.patch";
+      url = "https://github.com/epics-modules/sequencer/commit/de4f7c21fb73a4fe780fa25c78d725ac050409de.patch?full_index=1";
+      hash = "sha256-Sqso2NMVLw4GkCDCpp2AA9O444NueGt/bXkLXslcx5k=";
+    })
   ];
 
   preBuild = ''
