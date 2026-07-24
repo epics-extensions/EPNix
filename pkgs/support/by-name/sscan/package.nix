e6bcfcbec2b0efa74a69256621d54c85265a127e
Minijackson <minijackson@riseup.net>
2026-07-24 14:23:14 +0200

    support.sscan: fix C23 compilation

--- pkgs/support/by-name/sscan/package.nix
+++ pkgs/support/by-name/sscan/package.nix
@@ -6,15 +6,20 @@
 }:
 mkEpicsPackage (finalAttrs: {
   pname = "sscan";
-  version = "2-11-6";
+  version = "2-12";
   varname = "SSCAN";
 
   src = fetchFromGitHub {
     owner = "epics-modules";
-    repo = finalAttrs.pname;
+    repo = "sscan";
     tag = "R${finalAttrs.version}";
-    sha256 = "sha256-hrPap4FBKMD4ddMrADOeTAmsG+rLFxALibT3qsAHNsk=";
+    sha256 = "sha256-3ayoRUPBv8lNnM80VNRCMN6FHbq4csogbrFkwoPjHrI=";
   };
+
+  patches = [
+    # https://github.com/epics-modules/sscan/pull/41
+    ./fix-k-r-xdrproc-def.patch
+  ];
 
   buildInputs = [ seq ];
 
